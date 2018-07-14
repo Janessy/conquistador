50e8858fea5a13c6ce19f5873148c625e50c33b7
Janessy Cristina Ricaldi Astuhuaman <rjanessy@gmail.com>
2018-07-14 18:51:10 -0500

    conquistador

---
+++ app/Http/Controllers/ComidaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace conquistador\Http\Controllers;
+
+use Illuminate\Http\Request;
+use conquistador\Models\Comida;
+use Illuminate\Support\Facades\Redirect;
+use conquistador\Http\Requests\ComidaFormRequest;
+use DB;
+
+class ComidaController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index(Request $request)
+    {
+      if($request){
+        $query=trim($request->get('searchText'));
+        $comida=DB::table('foods')
+            ->where('name','LIKE','%'.$query.'%')
+            ->orderBy('idfoods','asc')
+            ->paginate(7);
+       return view('almacen.comida.index',["comida" =>$comida,"searchText"=>$query]);
+      }
+    }
+
+    public function create()
+    {
+        return view("almacen.comida.create");
+    }
+
+    public function store(ComidaFormRequest $request )
+    {
+        $comida= new Comida;
+        $comida->name=$request ->get('name');
+        $comida->precio=$request ->get('precio');
+        $comida->idcategoria=$request ->get('idcategoria');
+        $comida->save();
+       return Redirect::to('almacen/comida');
+    }
+
+    public function show($id)
+    {
+        return view("almacen.comida.show",["comida" =>Comida::findOrFail($id)]);
+
+    }
+
+    public function edit($id)
+    {
+        return view("almacen.comida.edit",["comida" =>Comida::findOrFail($id)]);
+    }
+
+    public function update(ComidaFormRequest $request,$id)
+    {
+        $comida=Comida::findOrFail($id);
+        $comida->nombre=$request -> get('nombre');
+        $comida->precio=$request -> get('precio');
+        $comida-> update();
+        return Redirect::to('almacen/comida');
+
+    }
+
+    public function destroy($id)
+    {
+        $comida=Comida::finOrFail($id);
+        $comida->update();
+      return Redirect::to('almacen/comida');
+
+    }
+}
---
+++ app/Http/Controllers/MesaController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace conquistador\Http\Controllers;
+
+use Illuminate\Http\Request;
+use conquistador\Models\Mesa;
+use Illuminate\Support\Facades\Redirect;
+use conquistador\Http\Requests\MesaFormRequest;
+use DB;
+
+class MesaController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index(Request $request)
+    {
+      if($request){
+        $query=trim($request->get('searchText'));
+        $mesa=DB::table('mesa')
+            ->where('mesa','LIKE','%'.$query.'%')
+            ->paginate(5);
+       return view('almacen.mesa.index',["mesa" =>$mesa,"searchText"=>$query]);
+      }
+    }
+
+    public function create()
+    {
+        return view("almacen.mesa.create");
+    }
+
+    public function store(MesaFormRequest $request )
+    {
+       $mesa= new Mesa;
+       $mesa -> mesa=$request ->get('mesa');
+       $mesa->save();
+       return Redirect::to('almacen/mesa');
+    }
+
+    public function show($id)
+    {
+        return view("almacen.mesa.show",["categoria" =>Mesa::findOrFail($id)]);
+
+    }
+
+    public function edit($id)
+    {
+        return view("almacen.mesa.edit",["categoria" =>Mesa::findOrFail($id)]);
+    }
+
+    public function update(MesaFormRequest $request,$id)
+    {
+        $mesa=Mesa::findOrFail($id);
+        $mesa->mesa=$request -> get('mesa');
+        $mesa-> update();
+        return Redirect::to('almacen/mesa');
+
+    }
+
+    public function destroy($id)
+    {
+      $mesa=Mesa::finOrFail($id);
+      $mesa->condicion='0';
+      $mesa->update();
+      return Redirect::to('almacen/categoria');
+
+    }
+}
